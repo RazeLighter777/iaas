1aab525dbea16eacd5656a8b090a44dfd3362ec9
Justin <justin@prizrak.me>
2025-03-08 20:26:46 +0000

    fixing vars

---
+++ cluster/platforms/authentik/terraform/authentik/variables.tf
@@ -0,0 +1,9 @@
+variable "AUTHENTIK_BOOTSTRAP_TOKEN" {
+  type = string
+  description = "Bootstrap token for authentik"
+}
+
+variable "domain_name" {
+  type = string
+  description = "Domain name for network"
+}
