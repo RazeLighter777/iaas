99d26ec2fc3f141d166da87a607549b9bef416ff
Justin <justin@prizrak.me>
2025-03-08 20:18:58 +0000

    groups

--- cluster/platforms/authentik/terraform/authentik/groups.tf
+++ cluster/platforms/authentik/terraform/authentik/groups.tf
@@ -1,16 +1,16 @@
-resource "authentik_group" "group" {
+resource "authentik_group" "jellyfin" {
   name         = "jellyfin"
 }
-resource "authentik_group" "group" {
+resource "authentik_group" "sonarr" {
   name         = "sonarr"
 }
-resource "authentik_group" "group" {
+resource "authentik_group" "radarr" {
   name         = "radarr"
 }
-resource "authentik_group" "group" {
+resource "authentik_group" "capacitor" {
   name         = "capacitor"
 }
-resource "authentik_group" "group" {
+resource "authentik_group" "sillytavern" {
   name         = "sillytavern"
 }
 
