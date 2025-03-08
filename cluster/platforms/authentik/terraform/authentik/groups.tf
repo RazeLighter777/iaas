1b0350948d9beefb1ccf070b135d89d5809d99d5
Justin <justin@prizrak.me>
2025-03-08 20:16:21 +0000

    group

---
+++ cluster/platforms/authentik/terraform/authentik/groups.tf
@@ -0,0 +1,16 @@
+resource "authentik_group" "group" {
+  name         = "jellyfin"
+}
+resource "authentik_group" "group" {
+  name         = "sonarr"
+}
+resource "authentik_group" "group" {
+  name         = "radarr"
+}
+resource "authentik_group" "group" {
+  name         = "capacitor"
+}
+resource "authentik_group" "group" {
+  name         = "sillytavern"
+}
+
