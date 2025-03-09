0eb3902c443e4426d944137c1bf1694a80a3e1d9
Justin <justin@prizrak.me>
2025-03-09 02:25:49 +0000

    fixing

--- cluster/platforms/authentik/terraform/authentik/sillytavern-app.tf
+++ cluster/platforms/authentik/terraform/authentik/sillytavern-app.tf
@@ -1,10 +1,9 @@
-module "radarr" {
+module "sillytavern" {
   source = "./modules/forward-auth-application"
-  slug   = "radarr"
-
-  name   = "Radarr"
+  slug   = "sillytavern"
+  name   = "Sillytavern"
   domain_name = "radarr.${var.domain_name}"
-  group  = "media"
+  group  = "AI Slop"
 
   policy_engine_mode      = "any"
   authorization_flow_uuid = data.authentik_flow.default-authorization-flow.id
@@ -12,12 +11,12 @@ module "radarr" {
   meta_icon = "https://git.zknt.org/mirror/Radarr/raw/commit/21ed073f294e8ab4d245f64dd6ae62f1c940b9f4/Logo/800.png"
 }
 
-resource "authentik_group" "radarr-users" {
+resource "authentik_group" "sillytavern-users" {
   name = "radarr-users"
 }
 
-resource "authentik_policy_binding" "radarr-users-binding" {
+resource "authentik_policy_binding" "sillytavern-users-binding" {
   target = module.radarr.application_id
-  group = authentik_group.radarr-users.id
+  group = authentik_group.sillytavern-users.id
   order = 0
 }
