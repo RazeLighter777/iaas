bf36520fac592f9cb958d0434236de2da5a14f53
RazeLighter777 <justin.suess@westpoint.edu>
2025-03-12 14:53:00 +0000

    moving secret

---
+++ cluster/platforms/authentik/terraform/grafana-app.tf
@@ -0,0 +1,59 @@
+data "authentik_flow" "default-provider-authorization-implicit-consent" {
+  slug = "default-provider-authorization-implicit-consent"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-email" {
+  name = "authentik default OAuth Mapping: OpenID 'email'"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-profile" {
+  name = "authentik default OAuth Mapping: OpenID 'profile'"
+}
+
+data "authentik_property_mapping_provider_scope" "scope-openid" {
+  name = "authentik default OAuth Mapping: OpenID 'openid'"
+}
+
+"variable" "grafana_client_id" {
+  description = "The client ID for the Grafana OAuth2 provider"
+}
+
+"variable" "grafana_client_secret" {
+  description = "The client secret for the Grafana OAuth2 provider"
+}
+
+resource "authentik_provider_oauth2" "grafana" {
+  name          = "Grafana"
+  client_id     = var.grafana_client_id
+  client_secret = var.grafana_client_secret
+
+  authorization_flow  = data.authentik_flow.default-provider-authorization-implicit-consent.id
+
+  redirect_uris = ["https://grafana.${var.domain_name}/login/generic_oauth"]
+
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope-email.id,
+    data.authentik_property_mapping_provider_scope.scope-profile.id,
+    data.authentik_property_mapping_provider_scope.scope-openid.id,
+  ]
+}
+
+resource "authentik_application" "grafana" {
+  name              = "Grafana"
+  slug              = "grafana"
+  protocol_provider = authentik_provider_oauth2.grafana.id
+}
+
+resource "authentik_group" "grafana_admins" {
+  name    = "Grafana Admins"
+}
+
+resource "authentik_group" "grafana_editors" {
+  name    = "Grafana Editors"
+}
+
+resource "authentik_group" "grafana_viewers" {
+  name    = "Grafana Viewers"
+}
+
+
