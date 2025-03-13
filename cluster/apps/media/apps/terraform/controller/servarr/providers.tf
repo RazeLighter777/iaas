51ba93a511afd38e68ff607fd7f03d61455c21f4
Justin <justin.suess@westpoint.edu>
2025-03-13 23:11:53 +0000

    terraform

---
+++ cluster/apps/media/apps/terraform/controller/servarr/providers.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = "1.11.1"
+  required_providers {
+    sonarr = {
+      source  = "devopsarr/sonarr"
+      version = "3.4.0"
+    }
+    prowlarr = {
+      source  = "devopsarr/prowlarr"
+      version = "3.0.2"
+    }
+    radarr = {
+      source  = "devopsarr/radarr"
+      version = "2.3.2"
+    }
+    external = {
+      source  = "hashicorp/external"
+      version = "~> 2"
+    }
+  }
+}
+
+
+
+provider "sonarr" {
+  url     = "https://sonarr.${var.local_domain}"
+  api_key = var.SONARR__AUTH__APIKEY
+}
+
+provider "radarr" {
+  url     = "https://radarr.${var.local_domain}"
+  api_key = var.RADARR__AUTH__APIKEY
+}
+
+provider "prowlarr" {
+  url     = "https://prowlarr.${var.local_domain}"
+  api_key = var.PROWLARR__AUTH__APIKEY
+}
