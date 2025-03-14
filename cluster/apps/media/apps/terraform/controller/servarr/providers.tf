f6458caf1bb78d4eb2c73110f6618fdf8137d743
Justin <justin.suess@westpoint.edu>
2025-03-14 01:08:43 +0000

    fixes

--- cluster/apps/media/apps/terraform/controller/servarr/providers.tf
+++ cluster/apps/media/apps/terraform/controller/servarr/providers.tf
@@ -1,14 +1,14 @@
 provider "sonarr" {
-  url     = "https://sonarr.${var.local_domain}"
+  url     = "https://sonarr.${var.domain_name}"
   api_key = var.SONARR__AUTH__APIKEY
 }
 
 provider "radarr" {
-  url     = "https://radarr.${var.local_domain}"
+  url     = "https://radarr.${var.domain_name}"
   api_key = var.RADARR__AUTH__APIKEY
 }
 
 provider "prowlarr" {
-  url     = "https://prowlarr.${var.local_domain}"
+  url     = "https://prowlarr.${var.domain_name}"
   api_key = var.PROWLARR__AUTH__APIKEY
 }
