3b4e2cd0de7bfd06ad9c8a6bcc2c1c3e43556723
Justin <justin.suess@westpoint.edu>
2025-03-14 00:34:29 +0000

    Fixes

--- cluster/apps/media/apps/terraform/controller/servarr/providers.tf
+++ cluster/apps/media/apps/terraform/controller/servarr/providers.tf
@@ -1,22 +1,22 @@
 terraform {
-  required_version = "1.11.1"
-  required_providers {
-    sonarr = {
-      source  = "devopsarr/sonarr"
-      version = "3.4.0"
-    }
-    prowlarr = {
-      source  = "devopsarr/prowlarr"
-      version = "3.0.2"
-    }
-    radarr = {
-      source  = "devopsarr/radarr"
-      version = "2.3.2"
-    }
-    external = {
-      source  = "hashicorp/external"
-      version = "~> 2"
-    }
+    required_version >= "0.12"
+    required_providers {
+      sonarr = {
+        source  = "devopsarr/sonarr"
+        version = "3.4.0"
+      }
+      prowlarr = {
+        source  = "devopsarr/prowlarr"
+        version = "3.0.2"
+      }
+      radarr = {
+        source  = "devopsarr/radarr"
+        version = "2.3.2"
+      }
+      external = {
+        source  = "hashicorp/external"
+        version = "~> 2"
+      }
   }
 }
 
