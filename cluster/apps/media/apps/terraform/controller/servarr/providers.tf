02bb61779159aa10fd9de823d23d259f4a7df85e
Justin <justin.suess@westpoint.edu>
2025-03-14 01:18:49 +0000

    fixing sonarr radarr url

--- cluster/apps/media/apps/terraform/controller/servarr/providers.tf
+++ cluster/apps/media/apps/terraform/controller/servarr/providers.tf
@@ -1,14 +1,14 @@
 provider "sonarr" {
-  url     = "https://sonarr.${var.domain_name}"
+  url     = "http://sonarr.${var.cluster_media_domain}:${var.ports["sonarr"]}"
   api_key = var.SONARR__AUTH__APIKEY
 }
 
 provider "radarr" {
-  url     = "https://radarr.${var.domain_name}"
+  url     = "http://radarr.${var.cluster_media_domain}:${var.ports["radarr"]}"
   api_key = var.RADARR__AUTH__APIKEY
 }
 
 provider "prowlarr" {
-  url     = "https://prowlarr.${var.domain_name}"
+  url     = "http://prowlarr.${var.cluster_media_domain}:${var.ports["prowlarr"]}"
   api_key = var.PROWLARR__AUTH__APIKEY
 }
