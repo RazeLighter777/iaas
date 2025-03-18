1f9199e7a689ad4ce8dd22c9cfaa45d2c9c45433
Justin <justin.suess@westpoint.edu>
2025-03-18 16:00:00 +0000

    pushing

--- terraform/servarr/radarr.tf
+++ terraform/servarr/radarr.tf
@@ -50,7 +50,6 @@ resource "radarr_notification_discord" "media_discord" {
   on_download                      = true
   on_upgrade                       = true
   on_rename                        = false
-  on_movie_added                   = false
   on_movie_delete                  = false
   on_movie_file_delete             = false
   on_movie_file_delete_for_upgrade = true
