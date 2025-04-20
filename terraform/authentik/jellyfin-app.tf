3a218957215bed7857f7cc9728153a6199b8f97a
RazeLighter777 nunyabuisness <justin@prizrak.me>
2025-04-20 11:01:01 -0400

    added jellyfin ldap

---
+++ terraform/authentik/jellyfin-app.tf
@@ -0,0 +1,55 @@
+
+resource "authentik_provider_ldap" "jellyfin" {
+  name                    = "Jellyfin LDAP"
+  base_dn                 = "dc=authentik,dc=${replace(var.domain_name, ".", ",dc=")}"
+  bind_flow               = data.authentik_flow.default-authentication-flow.id
+  search_group            = null
+  certificate             = null
+  
+  # LDAP specific settings
+  bind_mode               = "direct"
+  mfa_support             = false
+  
+  # User and group search settings
+  object_uniqueness_field = "uid"
+  user_path_template      = "goauthentik.io/users/%(username)s"
+  
+  # Attribute mappings
+  property_mappings_group = []
+  property_mappings_user  = [
+    data.authentik_property_mapping_ldap.name.id,
+    data.authentik_property_mapping_ldap.email.id,
+    data.authentik_property_mapping_ldap.uid.id
+  ]
+}
+
+data "authentik_property_mapping_ldap" "name" {
+  name = "authentik default LDAP Mapping: Name"
+}
+
+data "authentik_property_mapping_ldap" "email" {
+  name = "authentik default LDAP Mapping: Email"
+}
+
+data "authentik_property_mapping_ldap" "uid" {
+  name = "authentik default LDAP Mapping: User ID"
+}
+
+resource "authentik_application" "jellyfin" {
+  name              = "Jellyfin"
+  slug              = "jellyfin"
+  protocol_provider = authentik_provider_ldap.jellyfin.id
+  group             = "media"
+  meta_icon         = "https://jellyfin.org/images/logo.svg"
+  meta_launch_url   = "https://jellyfin.${var.domain_name}"
+}
+
+resource "authentik_group" "jellyfin-users" {
+  name = "jellyfin-users"
+}
+
+resource "authentik_policy_binding" "jellyfin-users-binding" {
+  target = authentik_application.jellyfin.uuid
+  group  = authentik_group.jellyfin-users.id
+  order  = 0
+}
--- terraform/authentik/system.tf
+++ terraform/authentik/system.tf
@@ -45,3 +45,37 @@ resource "authentik_outpost" "proxyoutpost" {
     kubernetes_image_pull_secrets  = []
   })
 }
+
+# for jellyfin
+resource "authentik_outpost" "ldapoutpost" {
+  name               = "ldap-outpost"
+  type               = "ldap"
+  service_connection = authentik_service_connection_kubernetes.local.id
+  protocol_providers = [
+    authentik_provider_ldap.jellyfin.id
+  ]
+  config = jsonencode({
+    authentik_host          = "https://authentik.${var.domain_name}",
+    authentik_host_insecure = false,
+    authentik_host_browser  = "",
+    log_level               = "debug",
+    object_naming_template  = "ak-outpost-%(name)s",
+    docker_network          = null,
+    docker_map_ports        = true,
+    docker_labels           = null,
+    container_image         = null,
+    kubernetes_replicas     = 1,
+    kubernetes_namespace    = "authentik",
+    kubernetes_ingress_annotations = {
+      "cert-manager.io/cluster-issuer" = "letsencrypt-issuer"
+    },
+    kubernetes_ingress_secret_name = "ldap-outpost-tls",
+    kubernetes_ingress_class_name     = "nginx",
+    kubernetes_service_type        = "ClusterIP",
+    kubernetes_disabled_components = [],
+    kubernetes_image_pull_secrets  = []
+  })
+}
+
+    
+
