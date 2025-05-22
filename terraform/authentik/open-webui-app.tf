8a63e607576b6f389716505e971336e798048c89
RazeLighter777 <gorgonballs@proton.me>
2025-05-22 15:28:28 -0400

    Add open-webui as an app to this repository
    adding openwebui.
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/RazeLighter777/iaas?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ terraform/authentik/open-webui-app.tf
@@ -0,0 +1,40 @@
+variable "open_webui_client_id" {
+  description = "The client ID for the Open WebUI OAuth2 provider"
+}
+
+variable "open_webui_client_secret" {
+  description = "The client secret for the Open WebUI OAuth2 provider"
+}
+
+resource "authentik_provider_oauth2" "open_webui" {
+  name          = "Open WebUI"
+  client_id     = var.open_webui_client_id
+  client_secret = var.open_webui_client_secret
+
+  authorization_flow  = data.authentik_flow.default-authorization-flow.id
+  invalidation_flow   = data.authentik_flow.default-provider-invalidation-flow.id
+  allowed_redirect_uris = [
+    {
+      matching_mode = "strict"
+      url           = "https://openwebui.${var.domain_name}/oauth/oidc/callback"
+    }
+  ]
+
+  property_mappings = [
+    data.authentik_property_mapping_provider_scope.scope-email.id,
+    data.authentik_property_mapping_provider_scope.scope-profile.id,
+    data.authentik_property_mapping_provider_scope.scope-openid.id,
+  ]
+}
+
+resource "authentik_application" "open_webui" {
+  name              = "Open WebUI"
+  slug              = "open-webui"
+  protocol_provider = authentik_provider_oauth2.open_webui.id
+  group             = "AI Slop"
+  meta_icon         = "https://s3.${var.domain_name}/media/open-webui.svg"
+}
+
+resource "authentik_group" "open_webui_users" {
+  name    = "Open WebUI Users"
+}
