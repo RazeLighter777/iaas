b6c1b9f2eef4902036a0440610f20bdf69f54013
Justin Suess <utilityemal77@gmail.com>
2026-02-12 13:11:51 -0500

    updating s3

--- containers/nixos-build-daemon/nix-post-build-hook.sh
+++ containers/nixos-build-daemon/nix-post-build-hook.sh
@@ -23,4 +23,4 @@ fi
 
 nix sign-paths --key-file "${NIX_CACHE_PRIVATE_KEY_FILE}" ${paths}
 
-nix copy --to "s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}" ${paths}
+nix copy --to "s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}&profile=${AWS_PROFILE:-default}" ${paths}
--- containers/nixos-build-daemon/start-nix-daemon.sh
+++ containers/nixos-build-daemon/start-nix-daemon.sh
@@ -5,6 +5,28 @@ mkdir -p /etc/nix /nix/var/nix/daemon-socket /nix/var/nix/builds
 chown root:root /nix/var/nix/builds
 chmod 0755 /nix/var/nix/builds
 
+mkdir -p "${HOME:-/root}/.aws"
+if [ -n "${AWS_ACCESS_KEY_ID:-}" ] && [ -n "${AWS_SECRET_ACCESS_KEY:-}" ]; then
+  cat >"${AWS_SHARED_CREDENTIALS_FILE:-${HOME:-/root}/.aws/credentials}" <<EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOF
+
+  region="${AWS_REGION:-${NIX_S3_REGION:-}}"
+  if [ -n "${region}" ]; then
+    cat >"${AWS_CONFIG_FILE:-${HOME:-/root}/.aws/config}" <<EOF
+[default]
+region = ${region}
+EOF
+  fi
+
+  chmod 0600 "${AWS_SHARED_CREDENTIALS_FILE:-${HOME:-/root}/.aws/credentials}"
+  if [ -f "${AWS_CONFIG_FILE:-${HOME:-/root}/.aws/config}" ]; then
+    chmod 0600 "${AWS_CONFIG_FILE:-${HOME:-/root}/.aws/config}"
+  fi
+fi
+
 trusted_keys="cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 if [ -n "${NIX_CACHE_PUBLIC_KEY:-}" ]; then
   trusted_keys="${trusted_keys} ${NIX_CACHE_PUBLIC_KEY}"
@@ -12,7 +34,7 @@ fi
 
 substituters="https://cache.nixos.org"
 if [ -n "${NIX_S3_BUCKET:-}" ] && [ -n "${NIX_S3_ENDPOINT:-}" ] && [ -n "${NIX_S3_REGION:-}" ]; then
-  substituters="${substituters} s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}"
+  substituters="${substituters} s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}&profile=${AWS_PROFILE:-default}"
 fi
 
 cat >/etc/nix/nix.conf <<EOF
