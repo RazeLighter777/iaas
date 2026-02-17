1e096f0c149e9982fb88235f67037b5d16f27950
Justin Suess <utilityemal77@gmail.com>
2026-02-17 12:55:48 -0500

    fixing gateway

--- containers/nixos-build-daemon/nix-post-build-hook.sh
+++ containers/nixos-build-daemon/nix-post-build-hook.sh
@@ -21,6 +21,6 @@ if [ -z "${paths}" ]; then
   exit 0
 fi
 
-nix sign-paths --key-file "${NIX_CACHE_PRIVATE_KEY_FILE}" ${paths}
 
 nix copy --to "s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}&profile=${AWS_PROFILE:-default}" ${paths}
+nix store sign --store "s3://${NIX_S3_BUCKET}?endpoint=${NIX_S3_ENDPOINT}&region=${NIX_S3_REGION}&scheme=${NIX_S3_SCHEME:-https}&profile=${AWS_PROFILE:-default}" --key-file "${NIX_CACHE_PRIVATE_KEY_FILE}" ${paths}
